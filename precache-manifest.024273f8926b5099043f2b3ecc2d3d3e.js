fa0b34ec5e29e9ab3d79efc530999d8f50280361
Kostiantyn Berehovyi <edifaer29122@gmail.com>
2020-07-16 14:27:29 +0300

    Updates

---
+++ precache-manifest.024273f8926b5099043f2b3ecc2d3d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812b62d41def6b41430f2e31d7c405c1",
+    "url": "/edifaer/todo-app/index.html"
+  },
+  {
+    "revision": "2fc1880291b437093fb4",
+    "url": "/edifaer/todo-app/static/css/main.d9d9bca9.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/edifaer/todo-app/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/edifaer/todo-app/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc1880291b437093fb4",
+    "url": "/edifaer/todo-app/static/js/main.d495e4d5.chunk.js"
+  },
+  {
+    "revision": "ba246de49ed1b8a13a21",
+    "url": "/edifaer/todo-app/static/js/runtime-main.99bea761.js"
+  }
+]);
